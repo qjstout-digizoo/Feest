de7111e62379a3af1ea667dfd43f68a6032cad94
QjStout <qj@digizoo.com.au>
2023-07-20 16:25:58 +1000

    Initial Commit

---
+++ Feest/FeestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeestApp.swift
+//  Feest
+//
+//  Created by Qj Stout-Spykers on 20/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct FeestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
